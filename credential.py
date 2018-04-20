651c2f30bf29d0391ba6c4ace7c930161d232432
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-04-21 00:19:48 +0530

    Create credential.py

---
+++ credential.py
@@ -0,0 +1,9 @@
+# SalesForce Credentails
+username='someone@company.com'
+password='blablabla'
+security_token='NSUgqlsdsfgerhvdsvVbwjs'
+
+# Gmail Credentials
+ORG_EMAIL= "@gmail.com"
+FROM_EMAIL= "someone@gmail.com"
+FROM_PWD= 'blablablabla'
