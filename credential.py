b9849732431a9cdadb7f4426eda2d8ac0edb1913
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-04-27 20:32:11 +0530

    Update credential.py

--- credential.py
+++ credential.py
@@ -1,4 +1,9 @@
-# SalesForce Credentails
+# Salesforce credentials
+
+# Find out Security Token:
+# https://<yoursalesforcehostname>/_ui/system/security/ResetApiTokenEdit?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DPersonalInfo&setupid=ResetApiToken
+# https://avi.my.salesforce.com/_ui/system/security/ResetApiTokenEdit?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DPersonalInfo&setupid=ResetApiToken
+
 username='someone@company.com'
 password='blablabla'
 security_token='NSUgqlsdsfgerhvdsvVbwjs'
